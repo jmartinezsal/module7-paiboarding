59f7f451e25e134f0dfea94633d9fe9e7e36b1e3
Jose Martinez Saldana <jmartinezsal326@gmail.com>
2022-07-25 15:24:23 -0700

    digital root

--- digitalRoot.js
+++ digitalRoot.js
@@ -5,3 +5,33 @@ Keep repeating until there is only one digit in the result,
  called the "digital root". Do not use string conversion
  within your method.
 */
+
+/* Psuedo code:
+  edge case - if num is less than 10 that is the result
+
+  start by having a total = 0
+  modululos the num by 10
+  now get integer division by Math.floor(num)
+
+*/
+
+function digitalRoot(num) {
+  while (num >= 10) {
+    num = digitalRootStep(num);
+  }
+
+  return num;
+}
+
+function digitalRootStep(num) {
+  let root = 0;
+
+  while (num > 0) {
+    root += num % 10;
+    num = Math.floor(num/10);
+  }
+
+  return root;
+}
+
+console.log(digitalRoot(46))
